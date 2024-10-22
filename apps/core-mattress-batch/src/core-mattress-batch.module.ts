2a00425276bf86cd9c319b9e22efda78c944f1f9
mumar9504 <minu0709ms@gmail.com>
2024-10-22 19:16:14 +0900

    feat: configure env variables

--- apps/core-mattress-batch/src/core-mattress-batch.module.ts
+++ apps/core-mattress-batch/src/core-mattress-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoreMattressBatchController } from './core-mattress-batch.controller';
 import { CoreMattressBatchService } from './core-mattress-batch.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [CoreMattressBatchController],
   providers: [CoreMattressBatchService],
 })
--- apps/core-mattress-batch/src/main.ts
+++ apps/core-mattress-batch/src/main.ts
@@ -3,6 +3,6 @@ import { CoreMattressBatchModule } from './core-mattress-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(CoreMattressBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
