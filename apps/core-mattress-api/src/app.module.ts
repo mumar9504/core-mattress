bda351d38d2869459188f71e767c052cef152a10
mumar9504 <minu0709ms@gmail.com>
2024-10-29 01:48:14 +0900

    feat: develop global GraphQL error handling system

--- apps/core-mattress-api/src/app.module.ts
+++ apps/core-mattress-api/src/app.module.ts
@@ -7,6 +7,7 @@ import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
 import { DatabaseModule } from './database/database.module';
+import { T } from './libs/types/common';
 
 @Module({
 	imports: [
@@ -14,8 +15,18 @@ import { DatabaseModule } from './database/database.module';
 		GraphQLModule.forRoot({
 			driver: ApolloDriver,
 			playground: true,
-      uploads: false,
-      autoSchemaFile: true,
+			uploads: false,
+			autoSchemaFile: true,
+			// GRAPHQL GLOBAL ERROR HANDLING
+			formatError: (error: T) => {
+				const graphQLFormattedError = {
+					code: error.extensions?.code,
+					message:
+						error?.extensions?.exception?.response?.message || error?.extensions?.response?.message || error?.message,
+				};
+				console.log('GRAPHQL GLOBAL ERR:', graphQLFormattedError);
+				return graphQLFormattedError;
+			},
 		}),
 		ComponentsModule,
 		DatabaseModule,
--- apps/core-mattress-api/src/components/member/member.service.ts
+++ apps/core-mattress-api/src/components/member/member.service.ts
@@ -19,10 +19,8 @@ export class MemberService {
 			//result.accessToken = await this.authService.createToken(result);
 			return result;
 		} catch (err) {
-			console.log('Error, Service.model:', err);
-      throw new BadRequestException(err);
-			//console.log('Error, Service.model:', err.message);
-			//throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
+			console.log('Error, Service.model:', err.message);
+			throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
 		}
 	}
 
