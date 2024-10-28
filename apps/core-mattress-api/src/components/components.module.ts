5053e054bda2ce36fa988d80e72e3be6da8e7bcc
mumar9504 <minu0709ms@gmail.com>
2024-10-28 22:08:09 +0900

    feat: create components related modules

--- apps/core-mattress-api/src/components/components.module.ts
+++ apps/core-mattress-api/src/components/components.module.ts
@@ -1,8 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MemberModule } from './member/member.module';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { CommentModule } from './comment/comment.module';
+import { LikeModule } from './like/like.module';
+import { ViewModule } from './view/view.module';
+import { FollowModule } from './follow/follow.module';
+import { BoardArticleModule } from './board-article/board-article.module';
 
 @Module({
-  imports: [MemberModule, ProductModule]
+	imports: [
+		MemberModule,
+		AuthModule,
+		ProductModule,
+		BoardArticleModule,
+		LikeModule,
+		ViewModule,
+    CommentModule,
+		FollowModule,
+	],
 })
 export class ComponentsModule {}
