d5e3058ef48be8371da4fb1f603d331299e3ea03
mumar9504 <minu0709ms@gmail.com>
2024-10-29 00:46:15 +0900

    feat: develop signup business logic

--- apps/core-mattress-api/src/components/member/member.service.ts
+++ apps/core-mattress-api/src/components/member/member.service.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { Member } from '../../libs/dto/member/member';
+import { MemberInput } from '../../libs/dto/member/member.input';
 
 @Injectable()
 export class MemberService {
-  constructor(@InjectModel('Member') private readonly memberModel: Model<null>) {}
+  constructor(@InjectModel('Member') private readonly memberModel: Model<Member>) {}
 
-  public async signup(): Promise<string> {
-    return 'signup executed!';
-  }
+  public async signup(input: MemberInput): Promise<Member> {
+		// hASH PASSWORD
+		//input.memberPassword = await this.authService.hashPassword(input.memberPassword);
+		try {
+			const result = await this.memberModel.create(input);
+			// Authentification via Token
+			//result.accessToken = await this.authService.createToken(result);
+			return result;
+		} catch (err) {
+			console.log('Error, Service.model:', err);
+      throw new BadRequestException(err);
+			//console.log('Error, Service.model:', err.message);
+			//throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
+		}
+	}
 
   public async login(): Promise<string> {
     return 'login executed!';
