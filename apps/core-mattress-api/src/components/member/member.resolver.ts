c9f8053ce7247f32527c77692597dfbfe906c569
mumar9504 <minu0709ms@gmail.com>
2024-10-31 00:00:07 +0900

    fix: modify updateMember GraphQL api

--- apps/core-mattress-api/src/components/member/member.resolver.ts
+++ apps/core-mattress-api/src/components/member/member.resolver.ts
@@ -9,6 +9,7 @@ import { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
 
 @Resolver()
 export class MemberResolver {
@@ -26,17 +27,9 @@ export class MemberResolver {
 		return await this.memberService.login(input);
 	}
 
-	// Authenticated (USER, AGENT, ADMIN => bu yerda faqatgina LOGIN bo'lgan user bo'lsa yetarli)
-	@UseGuards(AuthGuard)
-	@Mutation(() => String)
-	public async updateMember(@AuthMember('_id') memberId: ObjectId): Promise<String> {
-		console.log('Mutation: updateMember');
-		return await this.memberService.updateMember();
-	}
-
 	// Authenticanni tekshirish
 	@UseGuards(AuthGuard)
-	@Query(() => String)
+	@Mutation(() => String)
 	public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
 		console.log('Mutation: checkAuth');
 		//console.log(typeof memberId);
@@ -47,12 +40,23 @@ export class MemberResolver {
 	// Athorizationni tekshirish
 	@Roles(MemberType.USER, MemberType.AGENT)
 	@UseGuards(RolesGuard)
-	@Query(() => String)
+	@Mutation(() => String)
 	public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
 		console.log('Mutation: checkAuthRoles');
 		return `Hi ${authMember.memberNick}, you are ${authMember.memberType}(memberId: ${authMember._id})!`;
 	}
 
+	// Authenticated (USER, AGENT, ADMIN => bu yerda faqatgina LOGIN bo'lgan user bo'lsa yetarli)
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async updateMember(
+		@Args('input') input: MemberUpdate,
+		@AuthMember('_id') memberId: ObjectId, //
+	): Promise<Member> {
+		console.log('Mutation: updateMember');
+		delete input._id;
+		return await this.memberService.updateMember(memberId, input);
+	}
 
 	@Query(() => String)
 	public async getMember(): Promise<String> {
