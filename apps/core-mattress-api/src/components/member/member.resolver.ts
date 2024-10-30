be05e4928e520b6b38cd150b748f43c7d80b41e8
mumar9504 <minu0709ms@gmail.com>
2024-10-30 23:34:42 +0900

    feat: create checkAuthRoles for testing

--- apps/core-mattress-api/src/components/member/member.resolver.ts
+++ apps/core-mattress-api/src/components/member/member.resolver.ts
@@ -6,6 +6,9 @@ import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { MemberType } from '../../libs/enums/member.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 @Resolver()
 export class MemberResolver {
@@ -33,12 +36,21 @@ export class MemberResolver {
 
 	// Authenticanni tekshirish
 	@UseGuards(AuthGuard)
-	@Mutation(() => String)
+	@Query(() => String)
 	public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
 		console.log('Mutation: checkAuth');
 		//console.log(typeof memberId);
 		console.log('memberNick:', memberNick);
 		return `Hi ${memberNick}!`;
+	}
+
+	// Athorizationni tekshirish
+	@Roles(MemberType.USER, MemberType.AGENT)
+	@UseGuards(RolesGuard)
+	@Query(() => String)
+	public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
+		console.log('Mutation: checkAuthRoles');
+		return `Hi ${authMember.memberNick}, you are ${authMember.memberType}(memberId: ${authMember._id})!`;
 	}
 
 
@@ -51,7 +63,9 @@ export class MemberResolver {
 	/** ADMIN **/
 
 	// Authorization: ADMIN
-	@Query(() => String)
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation(() => String)
 	public async getAllMembersByAdmin(): Promise<string> {
 		return await this.memberService.getAllMembersByAdmin();
 	}
