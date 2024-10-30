1e246182f11aeb377642f12d782668b23b7ca46a
mumar9504 <minu0709ms@gmail.com>
2024-10-31 00:17:27 +0900

    feat: develop getMember GraphQL api

--- apps/core-mattress-api/src/components/member/member.resolver.ts
+++ apps/core-mattress-api/src/components/member/member.resolver.ts
@@ -10,6 +10,8 @@ import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()
 export class MemberResolver {
@@ -58,10 +60,13 @@ export class MemberResolver {
 		return await this.memberService.updateMember(memberId, input);
 	}
 
-	@Query(() => String)
-	public async getMember(): Promise<String> {
+	@Query(() => Member)
+	public async getMember(
+		@Args('memberId') input: string,
+	): Promise<Member> {
 		console.log('Query: getMember');
-		return await this.memberService.getMember();
+		const targetId = shapeIntoMongoObjectId(input);
+		return await this.memberService.getMember(targetId);
 	}
 
 	/** ADMIN **/
