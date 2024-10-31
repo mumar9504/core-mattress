3603b2987bf589f5d2f951ab9abe9e5193868011
mumar9504 <minu0709ms@gmail.com>
2024-10-31 22:54:18 +0900

    feat: develop getAllMembersByAdmin and updateMemberByAdmin APIs

--- apps/core-mattress-api/src/components/member/member.resolver.ts
+++ apps/core-mattress-api/src/components/member/member.resolver.ts
@@ -1,6 +1,6 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { AgentsInquiry, LoginInput, MemberInput } from '../../libs/dto/member/member.input';
+import { AgentsInquiry, LoginInput, MemberInput, MembersInquiry } from '../../libs/dto/member/member.input';
 import { Member, Members } from '../../libs/dto/member/member';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
@@ -31,7 +31,7 @@ export class MemberResolver {
 
 	// Authenticanni tekshirish
 	@UseGuards(AuthGuard)
-	@Mutation(() => String)
+	@Query(() => String)
 	public async checkAuth(@AuthMember('memberNick') memberNick: string): Promise<string> {
 		console.log('Mutation: checkAuth');
 		//console.log(typeof memberId);
@@ -42,7 +42,7 @@ export class MemberResolver {
 	// Athorizationni tekshirish
 	@Roles(MemberType.USER, MemberType.AGENT)
 	@UseGuards(RolesGuard)
-	@Mutation(() => String)
+	@Query(() => String)
 	public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
 		console.log('Mutation: checkAuthRoles');
 		return `Hi ${authMember.memberNick}, you are ${authMember.memberType}(memberId: ${authMember._id})!`;
@@ -86,15 +86,18 @@ export class MemberResolver {
 	// Authorization: ADMIN
 	@Roles(MemberType.ADMIN)
 	@UseGuards(RolesGuard)
-	@Mutation(() => String)
-	public async getAllMembersByAdmin(): Promise<string> {
-		return await this.memberService.getAllMembersByAdmin();
+	@Query(() => Members)
+	public async getAllMembersByAdmin(@Args('input') input: MembersInquiry): Promise<Members> {
+		console.log('Query: getAllMembersByAdmin');
+		return await this.memberService.getAllMembersByAdmin(input);
 	}
 
-	@Mutation(() => String)
-	public async updateMemberByAdmin(): Promise<string> {
+	@Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation(() => Member)
+	public async updateMemberByAdmin(@Args('input') input: MemberUpdate): Promise<Member> {
 		console.log('Mutation: updateMemberByAdmin');
-		return await this.memberService.updateMemberByAdmin();
+		return await this.memberService.updateMemberByAdmin(input);
 	}
 
 }
--- apps/core-mattress-api/src/libs/dto/member/member.input.ts
+++ apps/core-mattress-api/src/libs/dto/member/member.input.ts
@@ -1,9 +1,8 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
 import { IsIn, IsNotEmpty, IsOptional, Length, Min } from 'class-validator';
 import { MemberAuthType, MemberStatus, MemberType } from '../../enums/member.enum';
-import { availableAgentSorts } from '../../config';
+import { availableAgentSorts, availableMemberSorts } from '../../config';
 import { Direction } from '../../enums/common.enum';
-//import { Direction } from '../../enums/common.enum';
 
 @InputType()
 export class MemberInput {
@@ -76,43 +75,43 @@ export class AgentsInquiry {
 	search: AISearch;
 }
 
-// @InputType()
-// class MISearch {
-// 	@IsOptional()
-// 	@Field(() => MemberType, { nullable: true })
-// 	memberType?: MemberType;
+@InputType()
+class MISearch {
+	@IsOptional()
+	@Field(() => MemberType, { nullable: true })
+	memberType?: MemberType;
 
-// 	@IsOptional()
-// 	@Field(() => MemberStatus, { nullable: true })
-// 	memberStatus?: MemberStatus;
+	@IsOptional()
+	@Field(() => MemberStatus, { nullable: true })
+	memberStatus?: MemberStatus;
 
-// 	@IsOptional()
-// 	@Field(() => String, { nullable: true })
-// 	text?: string;
-// }
+	@IsOptional()
+	@Field(() => String, { nullable: true })
+	text?: string;
+}
 
-// @InputType()
-// export class MembersInquiry {
-// 	@IsNotEmpty()
-// 	@Min(1)
-// 	@Field(() => Int)
-// 	page: number;
+@InputType()
+export class MembersInquiry {
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	page: number;
 
-// 	@IsNotEmpty()
-// 	@Min(1)
-// 	@Field(() => Int)
-// 	limit: number;
+	@IsNotEmpty()
+	@Min(1)
+	@Field(() => Int)
+	limit: number;
 
-// 	@IsOptional()
-// 	@IsIn([availableMemberSorts])
-// 	@Field(() => String, { nullable: true })
-// 	sort?: string;
+	@IsOptional()
+	@IsIn([availableMemberSorts])
+	@Field(() => String, { nullable: true })
+	sort?: string;
 
-// 	@IsOptional()
-// 	@Field(() => Direction, { nullable: true })
-// 	direction?: Direction;
+	@IsOptional()
+	@Field(() => Direction, { nullable: true })
+	direction?: Direction;
 
-// 	@IsNotEmpty()
-// 	@Field(() => MISearch)
-// 	search: MISearch;
-// }
+	@IsNotEmpty()
+	@Field(() => MISearch)
+	search: MISearch;
+}
