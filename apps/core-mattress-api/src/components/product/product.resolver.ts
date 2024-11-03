cbe03975bf27aa3a788b7392664714a73b8206df
mumar9504 <minu0709ms@gmail.com>
2024-11-03 20:06:52 +0900

    feat: develop removeProductByAdmin GraphQL api

--- apps/core-mattress-api/src/components/product/product.resolver.ts
+++ apps/core-mattress-api/src/components/product/product.resolver.ts
@@ -94,4 +94,13 @@ export class ProductResolver {
 		input._id = shapeIntoMongoObjectId(input._id);
 		return await this.productService.updateProductByAdmin(input);
 	}
+
+  @Roles(MemberType.ADMIN)
+	@UseGuards(RolesGuard)
+	@Mutation(() => Product)
+	public async removeProductByAdmin(@Args('productId') input: string): Promise<Product> {
+		console.log('Mutation: removeProductByAdmin');
+		const productId = shapeIntoMongoObjectId(input);
+		return await this.productService.removeProductByAdmin(productId);
+	}
 }
--- apps/core-mattress-api/src/components/product/product.service.ts
+++ apps/core-mattress-api/src/components/product/product.service.ts
@@ -179,19 +179,6 @@ export class ProductService {
 		return result[0];
 	}
 
-  public async productStatsEditor(input: StatisticModifier): Promise<Product> {
-		const { _id, targetKey, modifier } = input;
-		return await this.productModel
-			.findByIdAndUpdate(
-				_id,
-				{
-					$inc: { [targetKey]: modifier },
-				},
-				{ new: true },
-			)
-			.exec();
-	}
-
   /** ADMIN **/
 
 	public async getAllProductsByAdmin(input: AllProductsInquiry): Promise<Products> {
@@ -248,5 +235,26 @@ export class ProductService {
 			});
 		}
 		return result;
+	}
+
+  public async removeProductByAdmin(productId: ObjectId): Promise<Product> {
+		const search: T = { _id: productId, productStatus: ProductStatus.DELETE };
+		const result = await this.productModel.findOneAndDelete(search).exec();
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+		return result;
+	}
+
+  public async productStatsEditor(input: StatisticModifier): Promise<Product> {
+		const { _id, targetKey, modifier } = input;
+		return await this.productModel
+			.findByIdAndUpdate(
+				_id,
+				{
+					$inc: { [targetKey]: modifier },
+				},
+				{ new: true },
+			)
+			.exec();
 	}
 }
